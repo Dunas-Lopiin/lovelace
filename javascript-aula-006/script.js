ab72625bdf8d503e48533f0ca5095d03997bffc0
Plinio Figueiredo <91341276+Dunas-Lopiin@users.noreply.github.com>
2021-11-18 22:04:15 -0300

    aula document.getElementById

---
+++ javascript-aula-006/script.js
@@ -0,0 +1,28 @@
+/* Titulo e informações pessoais */
+
+document.getElementById("titulo").innerHTML = "RPG de mesa - Meu maior hobby!"
+document.getElementById("fotoTitulo").src = "imagens/eu.jpg"
+document.getElementById("nome").innerHTML = "Nome: Plinio Figueiredo dos Santos"
+document.getElementById("idade").innerHTML = "Idade: 26 Anos"
+document.getElementById("local").innerHTML = "Cidade / Estado: São Paulo - SP"
+
+/* Artigo sobre o rpg */
+
+document.getElementById("tituloRpg").innerHTML = "O que é o RPG de mesa?"
+document.getElementById("imaginacao").src = "imagens/imaginacao.jpeg"
+document.getElementById("sobre1").innerHTML = "O RPG de mesa é um tipo de jogo que funciona como uma história colaborativa, onde cada jogador toma comando de um personagem, realizando decisões de acordo com a personalidade do personagem. Normalmente um dos jogadores assumirá o papel de Mestre (ou narrador dependendo do jogo), ele ou ela é quem descreve o mundo aos outros jogadores e controla os personagens que interagirão com eles, sejam eles aliados, inimigos ou somente pessoas que vivem no mundo. Essencialmente o mestre é como os olhos e ouvidos dos personagens."
+document.getElementById("sobre2").innerHTML = "O legal do RPG de mesa é que ele permite que você crie um mundo totalmente novo com seu grupo de amigos, o que pode gerar histórias serias, com muita tensão e emoção, e histórias engraçadas, com piadas, situações aleatórias, etc. O limite é só a imaginação dos jogadores!"
+document.getElementById("sobre3").innerHTML = "Atualmente é extremamente fácil começar no hobby! Existe uma grande variedade de jogos (gratuitos e pagos), diversos materiais sobre o tema, como canais no youtube e artigos na net que deixam a vida dos iniciantes cada vez mais fácil. E com o advento da internet formar uma mesa para jogar nunca foi tão simples, você e seus amigos podem jogar mesmos separados por diversos quilômetros, países, oceanos, etc."
+
+/* Links sobre como começar no hobby */
+document.getElementById("tituloLinks").innerHTML = "Quer começar a jogar?"
+document.getElementById("jogatina").src = "imagens/jogatina.jpg"
+document.getElementById("informacoes").innerHTML = "Se interessou em jogar RPG de mesa? Abaixo temos alguns links para você começar a se aventurar por esse mundo, desde vídeos sobre o assunto até alguns jogos gratuitos que você pode querer testar, então boa sorte e BOM JOGO!"
+document.getElementById("link1").href ="https://www.youtube.com/playlist?list=PLG0aL-SDkL7HIJpG20Rjz_EzUY2dLq2zh"
+document.getElementById("link1").innerHTML = "Playlist do Mestre taverneiro para quem quer começar a jogar!"
+document.getElementById("link2").href ="https://dungeonist.com/catalog/role-playing-games/?features_hash=1-0-0-BRL"
+document.getElementById("link2").innerHTML = "Dungeonist, uma loja online onde você pode achar diversos RPGs gratuitos!"
+document.getElementById("link3").href ="https://castelo-arcano.itch.io/caminho-do-aventureiro"
+document.getElementById("link3").innerHTML = "Caminho do aventureiro, um rpg gratuito feito por mim que é tão pequeno que cabe um um cartão de visita!"
+document.getElementById("link4").href ="https://roll20.net/welcome"
+document.getElementById("link4").innerHTML = "Roll 20 - Uma plataforma gratuita que permite que você jogue RPG online!"
