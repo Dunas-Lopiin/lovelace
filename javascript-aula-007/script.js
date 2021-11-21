ab01b4a0c9a0114f2db1d6ab1a8d7cb462661ad2
Plinio Figueiredo dos Santos <91341276+Dunas-Lopiin@users.noreply.github.com>
2021-11-21 19:50:58 -0300

    atividade banda favorita

---
+++ javascript-aula-007/script.js
@@ -0,0 +1,5 @@
+document.querySelector("#banda img").src = "imagens/banda.jpg";
+document.querySelector("#banda img").style.border = "double 6px white";
+document.querySelector("#banda h2").style.color = "red";
+document.querySelector("#banda b").style.fontSize = "1.7em";
+document.querySelector(".favoritas").style.border = "double 6px red";
