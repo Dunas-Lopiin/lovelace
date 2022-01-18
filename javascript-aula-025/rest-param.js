95161d1b679bc69cd213c675848591674281b9c4
Plinio Figueiredo dos Santos <91341276+Dunas-Lopiin@users.noreply.github.com>
2022-01-18 09:16:16 -0300

    javascript-aula-025 - rest parameters

---
+++ javascript-aula-025/rest-param.js
@@ -0,0 +1,6 @@
+function novoArray(..._param){
+    console.log(_param);
+    return _param;
+}
+
+const nomes = novoArray("Juca", "Pedro", "Caio", "Elise", "Fernando", "dingo", "Crash", "Neo Cortex");
