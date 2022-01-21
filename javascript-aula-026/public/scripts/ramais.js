c63dcc130d08f0087ce7458273cb858eb32820c9
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-01-21 11:22:15 -0300

    Atualização do codigo dos ramais

--- javascript-aula-026/public/scripts/ramais.js
+++ javascript-aula-026/public/scripts/ramais.js
@@ -1,16 +1,21 @@
 module.exports.organizarRamais = (_lista) => {
     //codigo para organizar por nome
-    /* let organizado = _lista.sort(function (x, y) {
-            let a = x.Nome.toUpperCase(),
-            b = y.Nome.toUpperCase();
-        return a == b ? 0 : a > b ? 1 : -1;
-        }); */
+    let organizado = _lista.sort(function (x, y) {
+        let a = x.Nome.toUpperCase(),
+        b = y.Nome.toUpperCase();
+    return a == b ? 0 : a > b ? 1 : -1;
+    }); 
 
         //codigo para organizar por ramal
+        /*
         let organizado = _lista.sort(function (x, y) {
             let a = x.Ramal,
             b = y.Ramal;
         return a == b ? 0 : a > b ? 1 : -1;
-        });  
-    return organizado;
+        });
+*/
+    let somenteRamal = organizado.map(function(_elemento){
+        return [_elemento.Nome, _elemento.Ramal];
+    })  
+    return somenteRamal;
 }
